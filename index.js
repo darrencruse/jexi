18b4047a9f9fec39b38e4626fa5cb3b610f0f582
Darren Cruse <darren.cruse@monsanto.com>
2020-05-29 13:01:11 -0500

    converted to es6 modules and fixed lint errors

--- index.js
+++ index.js
@@ -1,120 +1,122 @@
-// will change to es6 module later the node repl is giving me trouble
+/* eslint-disable no-extra-parens */
+
+// note the base operations and special forms are the simple built-in ones
+// the application extends these using interpreter options
+import baseOperations from './operations.js'
+import baseSpecials from './specialForms.js'
 
 const interpreter = (options = {}) => {
-    //options.trace = true
+  //options.trace = true
 
-    const trace = (...args) => options.trace && console.log.apply(null, args)
+  // eslint-disable-next-line no-console
+  const trace = (...args) => options.trace && console.log.apply(null, args)
 
-    // our "symbols" in JSON strings marked with a prefix "$"
-    // but being sure to ignore money e.g. "$1.50" or syntax that may
-    // arise when embedding *other* expression languages e.g.
-    // jsonpath ("$.prop"), jsonata ("$max(array)") etc.
-    const symbolDetector = options.symbolDetector || '^\\$[^\$0-9][^(){}]*$'
-    const symRegex = new RegExp(symbolDetector);
-    // I originally used this to say $ followed by valid javascript var name,
-    // but I changed to the above to support things like "$>=", "$!==" etc.
-    //const symRegex = new RegExp('^\\' + symbolChar + '[A-Z_][0-9A-Z_$]*$', 'i');
- 
-    const isSymbol = atom => typeof atom === 'string' && symRegex.test(atom)
+  // our "symbols" in JSON strings marked with a prefix "$"
+  // but being sure to ignore money e.g. "$1.50" or syntax that may
+  // arise when embedding *other* expression languages e.g.
+  // jsonpath ("$.prop"), jsonata ("$max(array)") etc.
+  const symbolDetector = options.symbolDetector || '^\\$[^$0-9][^(){}]*$'
+  const symRegex = new RegExp(symbolDetector)
+  const isSymbol = atom => typeof atom === 'string' && symRegex.test(atom)
 
-    // unquoting the symbol string just removes the $ prefix
-    const unquoteSymbol = symbolStr => symbolStr.substring(1)
+  // converting symbol to string just removes the $ prefix
+  const symbolToString = symbolStr => symbolStr.substring(1)
 
-    // special forms
-    const baseSpecials = require('./specialForms.js')
-    const specialForms = { ...baseSpecials, ...(options.specialForms || {}) }
+  const specialForms = { ...baseSpecials, ...(options.specialForms || {}) }
 
-    trace('specialForms=', specialForms)
+  trace('specialForms=', specialForms)
 
-    // our registry of built in functions
-    const baseOperations = require('./operations.js')
-    const registry = { ...baseOperations, ...(options.operations || {}) }
+  const registry = { ...baseOperations, ...(options.operations || {}) }
 
-    trace('registry=', registry)
+  trace('registry=', registry)
 
-    const globals = {
-        params: { id: '1234567'},
-        models: { AGENCY_REQUEST: { 'blah': 'blah' } }
+  const interpret = (form, context = {}) => {
+    trace('interpret: evaluating:', JSON.stringify(form, null, 2))
+    if (Array.isArray(form)) {
+      // eslint-disable-next-line no-use-before-define
+      return interpretArrayForm(form, context)
+    } else if (typeof form === 'object') {
+      // eslint-disable-next-line no-use-before-define
+      return interpretObjectForm(form, context)
+    } else if (isSymbol(form)) {
+      trace(`interpret: replacing "${form}"/"${symbolToString(form)}" value from context or registry`)
+
+      // change to do these lookup using lodash get?
+      // like what about reaching into nested objects in the assembly state?
+      return context[form] || context[symbolToString(form)] || registry[form]
     }
 
-    // the array form is really the classic lispy s-expression form
-    const interpretArrayForm = (sform, context) => {
-        // special forms are called with their args unevaluated:
-        if (sform.length > 0 && specialForms[sform[0]]) {
-            return specialForms[sform[0]](sform, context, interpret, trace);
-        } else {
-            // regular forms have their args evaluated before they are called
-            const possibleSym = sform[0]
-            const evaluated = sform.map(atom => interpret(atom, context));
-            trace('sform: evaluated before call:', JSON.stringify(evaluated, null, 2), evaluated[0], typeof evaluated[0])
-            if (typeof evaluated[0] === 'function') {
-                trace(`sform: calling ${possibleSym}`)
-                return evaluated[0].call(undefined, evaluated, context, interpret, trace);
-            } else {
-                trace(`sform: ${possibleSym} not a function passing thru as evaluated data`)
-                if (isSymbol(possibleSym) && typeof evaluated[0] === 'undefined') {
-                    // this was an unrecognized symbol pass it thru unchanged
-                    evaluated[0] = possibleSym
-                }
-                return evaluated;
-            }
-        }
+    trace(`interpret: passing ${form} thru as plain data`)
+
+    return form
+  }
+
+  // the array form is really the classic lispy s-expression form
+  const interpretArrayForm = (sform, context) => {
+    // special forms are called with their args unevaluated:
+    if (sform.length > 0 && specialForms[sform[0]]) {
+      return specialForms[sform[0]](sform, context, interpret, trace)
     }
 
-    // the object form is using json object keys like named arguments
-    const interpretObjectForm = (oform, context) => {
-        // special forms are called with their args unevaluated:
-        const keys = Object.keys(oform) || []
-        const symbol = keys.find(isSymbol)
-        if (symbol) {
-            if (specialForms[symbol]) {
-                return specialForms[symbol](oform, context, interpret, interpret, trace);
-            } else if (typeof registry[symbol] === 'function') {
-                // regular sforms have their args evaluated before they are called
-                // the equivalent here is the values of all keys are evaluated before the fn is called
-                keys.forEach(key => {
-                    oform[key] = interpret(oform[key], context);
-                })
+    // regular forms have their args evaluated before they are called
+    const possibleSym = sform[0]
+    const evaluated = sform.map(atom => interpret(atom, context))
 
-                trace(`oform: calling ${symbol}`)
-                return registry[symbol].call(undefined, oform, context, interpret, trace);
-            } else {
-                trace(`passing thru object with unrecognized symbol key "${symbol}"`)
-            }               
-        }
+    trace('sform: evaluated before call:', JSON.stringify(evaluated, null, 2), evaluated[0], typeof evaluated[0])
+    if (typeof evaluated[0] === 'function') {
+      trace(`sform: calling ${possibleSym}`)
 
-        trace('oform: evaluting key values of plain of object as a template')
+      return evaluated[0].call(undefined, evaluated, context, interpret, trace)
+    }
+
+    trace(`sform: ${possibleSym} not a function passing thru as evaluated data`)
+    if (isSymbol(possibleSym) && typeof evaluated[0] === 'undefined') {
+      // this was an unrecognized symbol pass it thru unchanged
+      evaluated[0] = possibleSym
+    }
+
+    return evaluated
+  }
+
+  // the object form is using json object keys like named arguments
+  const interpretObjectForm = (oform, context) => {
+    // special forms are called with their args unevaluated:
+    const keys = Object.keys(oform) || []
+    const symbol = keys.find(isSymbol)
+
+    if (symbol) {
+      if (specialForms[symbol]) {
+        return specialForms[symbol](oform, context, interpret, interpret, trace)
+      } else if (typeof registry[symbol] === 'function') {
+        // regular sforms have their args evaluated before they are called
+        // the equivalent here is the values of all keys are evaluated before the fn is called
         keys.forEach(key => {
-            oform[key] = interpret(oform[key], context);
+          oform[key] = interpret(oform[key], context)
         })
 
-        return oform
+        trace(`oform: calling ${symbol}`)
+
+        return registry[symbol].call(undefined, oform, context, interpret, trace)
+      }
+
+      trace(`passing thru object with unrecognized symbol key "${symbol}"`)
     }
 
-    const interpret = (form, context = globals) => {
-        trace('interpret: evaluating:', JSON.stringify(form, null, 2))
-        if (Array.isArray(form)) {
-            return interpretArrayForm(form, context)
-        } else if (typeof form === 'object') {
-            return interpretObjectForm(form, context)
-        } else if (isSymbol(form)) {
-            trace(`interpret: replacing "${form}"/"${unquoteSymbol(form)}" value from context or registry`)
-    // change to do these lookup using lodash get?
-    // like what about reaching into nested objects in the assembly state?
-            return context[form] || context[unquoteSymbol(form)] || registry[form]
-        } else {
-            trace(`interpret: passing ${form} thru as plain data`)
-            return form
-        }
-    }
+    trace('oform: evaluting key values of plain of object as a template')
+    keys.forEach(key => {
+      oform[key] = interpret(oform[key], context)
+    })
 
-    return {
-        isSymbol,
-        interpret,
-        eval: interpret,
-    }
+    return oform
+  }
+
+  return {
+    isSymbol,
+    interpret,
+    eval: interpret,
+  }
 }
 
-module.exports = {
-    interpreter,
+export default {
+  interpreter,
 }
--- specialForms.js
+++ specialForms.js
@@ -1,70 +1,73 @@
+/* eslint-disable implicit-arrow-linebreak */
 const getArg = (form, name, pos) => Array.isArray(form) ? form[pos] : form[name]
 
-module.exports = {
-    // $let defines a scope with one or more local variables
-    // define one: ["$let", ["$x", 1], [body uses "$x" ]]
-    // define multiple: ["$let", [["$x", 1], ["$y", 2]], [body uses "$x" and "$y"]]
-    $let: (form, context, interpret, trace) => {
-        trace('in let:', form)
-        const letContext = Object.create(context)
-        // it's annoying when only defining one var to have to do wrap the var defs:
-        // e.g. ['$let', [['$x', 'hello world']], '$x'] - so wrap it for them
-        const varDefs = (form[1].length > 0 && Array.isArray(form[1][0])) ? form[1] : [form[1]]
+export default {
+  // $let defines a scope with one or more local variables
+  // define one: ["$let", ["$x", 1], [body uses "$x" ]]
+  // define multiple: ["$let", [["$x", 1], ["$y", 2]], [body uses "$x" and "$y"]]
+  $let: (form, context, interpret, trace) => {
+    trace('in let:', form)
+    const letContext = Object.create(context)
+    // it's annoying when only defining one var to have to do wrap the var defs:
+    // e.g. ['$let', [['$x', 'hello world']], '$x'] - so wrap it for them
+    const varDefs = form[1].length > 0 && Array.isArray(form[1][0]) ? form[1] : [ form[1] ]
 
-        varDefs.forEach(([name, value]) => {
-            trace('setting', name, 'to:', value)
-            letContext[name] = interpret(value, context);
-            trace('read back:', letContext[name])
-        })
+    varDefs.forEach(([ name, value ]) => {
+      trace('setting', name, 'to:', value)
+      letContext[name] = interpret(value, context)
+      trace('read back:', letContext[name])
+    })
 
-        return interpret(form[2], letContext);
-    },
+    return interpret(form[2], letContext)
+  },
 
-    // $var defines a variable in the current scope
-    // e.g. ["$var", "$x", 1]
-    $var: (form, context, interpret, trace) => {
-        const name = Array.isArray(form) ? form[1] : form["$var"][0]
-        const value = Array.isArray(form) ? form[2] : form["$var"][1]
-        trace(`setting ${name} to ${value}`)      
-        context[name] = interpret(value, context);
+  // $var defines a variable in the current scope
+  // e.g. ["$var", "$x", 1] or { "$var": [ "$x": 1 ]}
+  $var: (form, context, interpret, trace) => {
+    const name = Array.isArray(form) ? form[1] : form.$var[0]
+    const value = Array.isArray(form) ? form[2] : form.$var[1]
 
-        // we intentionally evaluate to undefined here
-        // because $var defining a function was getting called
-        // at the declaration
-        return undefined
-    },
+    trace(`setting ${name} to ${value}`)
+    context[name] = interpret(value, context)
 
-    // $=> = anonymous function
-    // ["$=>", [ "$arg1", ..., "$argN"], [ function body ]]
-    "$=>": (declareForm, declareContext, interpret, trace) => {
-        trace('declaring the lambda:', {declareForm, declareContext, interpret})
-        // return a function called later when the $fn is actually called  
-        return (callForm, callContext) => {
-            trace('handling the call:', callForm)
-            // put the values passed for the arguments into a local scope
-            const localContext= Object.create(callContext)
-            declareForm[1].forEach((argname, i) => {
-                // +1 below because the function is position 0
-                trace('setting argument in local scope:', argname, callForm[i+1])
-                localContext[argname] = callForm[i+1];
-            });
+    // we intentionally evaluate to undefined here
+    // because $var defining a function was getting called
+    // at the declaration
+    return undefined
+  },
 
-            // evaluate the body of the function with it's args in scope:
-            return interpret(declareForm[2], localContext);
-        };
-    },
+  // $=> = anonymous function
+  // ["$=>", [ "$arg1", ..., "$argN"], [ function body ]]
+  '$=>': (declareForm, declareContext, interpret, trace) => {
+    trace('declaring the lambda:', { declareForm, declareContext, interpret })
 
-    // $function = a named function
-    // ["$function", "$name", [ "$arg1", ..., "$argN"], [ function body ]]
-    $function: (form, context, interpret) =>
-        interpret(["$var", form[1], ["$=>", form[2], form[3]]], context),
-    
-    // $if is a special form because it only evaluates one of the if/else clauses
-    // e.g. ["$if", ["$>", "$x", 0], "some", "none"]
-    // or { "$if" : ["$>", "$x", 0], "then": "some", "else": "none"}
-    $if: (form, context, interpret) => {
-        return interpret(getArg(form, '$if', 1), context) ?
-            interpret(getArg(form, 'then', 2), context) :
-            interpret(getArg(form, 'else', 3), context)
+    // return a function called later when the $fn is actually called  
+    return (callForm, callContext) => {
+      trace('handling the call:', callForm)
+      // put the values passed for the arguments into a local scope
+      const localContext = Object.create(callContext)
+
+      declareForm[1].forEach((argname, i) => {
+        // +1 below because the function is position 0
+        trace('setting argument in local scope:', argname, callForm[i + 1])
+        localContext[argname] = callForm[i + 1]
+      })
+
+      // evaluate the body of the function with it's args in scope:
+      return interpret(declareForm[2], localContext)
     }
+  },
+
+  // $function = a named function
+  // ["$function", "$name", [ "$arg1", ..., "$argN"], [ function body ]]
+  $function: (form, context, interpret) =>
+    interpret([ '$var', form[1], [ '$=>', form[2], form[3] ]], context),
+
+  // $if is a special form because it only evaluates one of the if/else clauses
+  // e.g. ["$if", ["$>", "$x", 0], "some", "none"]
+  // or { "$if" : ["$>", "$x", 0], "then": "some", "else": "none"}
+  $if: (form, context, interpret) =>
+    interpret(getArg(form, '$if', 1), context) ?
+      interpret(getArg(form, 'then', 2), context) :
+      interpret(getArg(form, 'else', 3), context),
 }
