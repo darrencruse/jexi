f400a1c88c9cc78bf0c34c7d226e998951174c12
Darren Cruse <darren.cruse@monsanto.com>
2022-12-27 20:32:59 -0600

    latest changes from trying jexi in assembler project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-/* eslint-disable no-extra-parens, no-underscore-dangle */
+/* eslint-disable no-extra-parens, no-underscore-dangle, no-undef-init */
 import builtins from './registry.js'
 import get from 'lodash.get'
 import repl from 'repl'
+import util from 'util'
 
 const getRegistrySpecialForms = registry => {
   if (!registry || !registry.specialForms) {
     return {}
   }
 
-  // the "specialForms" section of the registry is just a convenience
-  // (they wind up in global scope with the other functions but marked _special)
+  // the "specialForms" are marked _special so we know not to eval their arguments
   Object.keys(registry.specialForms).forEach(key => {
     registry.specialForms[key]._special = true
   })
@@ -17,13 +17,44 @@ const getRegistrySpecialForms = registry => {
   return registry.specialForms
 }
 
-const getRegistryFunctions = registry => registry && registry.functions || {}
+const getRegistryFunctions = registry => {
+  if (!registry || !registry.functions) {
+    return {}
+  }
+
+  // the "functions" are marked _formhandler since they receive and
+  // handle the array and object form data directly.  this distinguishes
+  // them from plain javascript functions (e.g. console.log) which can't
+  // take the form data as arguments 
+  Object.keys(registry.functions).forEach(key => {
+    registry.functions[key]._formhandler = true
+  })
+
+  return registry.functions
+}
+
+// "globals" are plain javacript values added to the global variable scope
+const getRegistryGlobals = registry => registry && registry.globals || {}
+
+// a helper for getting an argument from a form,
+// name is the key to read for the object form, pos is the arg index in the array form 
+const getArg = (form, name, pos) => Array.isArray(form) ? form[pos] : form[name]
 
 export const interpreter = (options = {}) => {
-  //options.trace = true
+  // options.trace = true
+
+  const globals = {
+    trace: options.trace,
+    ...getRegistrySpecialForms(builtins),
+    ...getRegistrySpecialForms(options.registry),
+    ...getRegistryFunctions(builtins),
+    ...getRegistryFunctions(options.registry),
+    ...getRegistryGlobals(builtins),
+    ...getRegistryGlobals(options.registry),
+  }
 
   // eslint-disable-next-line no-console
-  const trace = (...args) => options.trace && console.log.apply(null, args)
+  const trace = (...args) => globals.trace && console.log.apply(null, args)
 
   // our "symbols" in JSON strings marked with a prefix "$"
   // but being sure to ignore money e.g. "$1.50" or syntax that may
@@ -33,13 +64,13 @@ export const interpreter = (options = {}) => {
   const isSymbol = atom => typeof atom === 'string' && symRegex.test(atom)
 
   // converting symbol to string just removes the $ prefix
-  const symbolToString = symbolStr => symbolStr.substring(1)
+  const symbolToString = symbolStr => isSymbol(symbolStr) ? symbolStr.substring(1) : String(symbolStr)
 
   // converting string to symbol just adds the $ prefix
-  const stringToSymbol = str => `${str.trim()}`
+  const stringToSymbol = str => !isSymbol(str) ? `$${str.trim()}` : str
 
-  // return the "$function" symbol specified in the provided (array or object) form
-  // (otherwise null if there isn't one)
+  // return the "$function" symbol specified in the provided array ("sform")
+  // or object ("oform") form.  Otherwise null if there isn't one
   const getFnSymbolForForm = form => {
     let fnSymbol = null
 
@@ -79,15 +110,6 @@ export const interpreter = (options = {}) => {
     return results
   }
 
-  const globals = {
-    ...getRegistrySpecialForms(builtins),
-    ...getRegistrySpecialForms(options.registry),
-    ...getRegistryFunctions(builtins),
-    ...getRegistryFunctions(options.registry),
-  }
-
-  trace('interpreter globals=', globals)
-
   const evaluate = async (form, variables = globals, options = {}) => {
     trace('evaluate: evaluating:', JSON.stringify(form, null, 2))
     //trace('evaluate: evaluating with vars of:', JSON.stringify(variables, null, 2))
@@ -100,11 +122,12 @@ export const interpreter = (options = {}) => {
       // eslint-disable-next-line no-use-before-define
       result = await evaluateObjectForm(form, variables, options)
     } else if (isSymbol(form)) {
-      trace(`evaluate: replacing "${form}"/"${symbolToString(form)}" value from variables`)
+      trace(`evaluate: replacing "${form}" with variables.${symbolToString(form)}`)
 
-      // note that we've made including the "$" prefix on the *actual* variable names
-      // optional (this might change TBD)
-      result = get(variables, form) || get(variables, symbolToString(form))
+      // note that we've chosen to omit the "$" prefix on the *actual* keys used in variables
+      // (this was done to make variables set by the interpreter and variables set by plain
+      // javascript be consistent rather than interpreter ones starting "$" and others not)
+      result = get(variables, symbolToString(form))
     } else {
       trace(`evaluate: passing ${form} thru as plain data`)
     }
@@ -117,29 +140,42 @@ export const interpreter = (options = {}) => {
   const startRepl = (options = {}) => {
     // TBD SHOULD I BE USING THEIR PROVIDED CONTEXT OR WIRING IT TO OURS SOMEHOW?
     const replEval = async (cmd, _context, _filename, callback) => {
-      let form = null
-      let result = null
+      let form = undefined
+      let result = undefined
 
-      try {
-        form = JSON.parse(cmd)
-      } catch (err) {
-        result = 'Invalid JSON please correct.'
-      }
+      if (cmd && cmd.trim()) {
+        try {
+          form = JSON.parse(cmd.trim())
+        } catch (err) {
+          result = 'Invalid JSON please correct.'
+        }
 
-      if (form) {
-        result = await evaluate(form)
+        if (form) {
+          result = await evaluate(form)
+          if (Array.isArray(result) && result.length > 0 && result[0].then) {
+            result = await Promise.all(result)
+          }
+        }
       }
 
       callback(null, result)
     }
 
-    repl.start({ prompt: `${options.prompt || '>'} `, eval: replEval })
+    // fix the repl default showing [Object]/[Array] with JSON > 2 levels deep:
+    const writeFullDepth = output => util.inspect(output, { depth: 100, colors: true })
+
+    repl.start({
+      eval: replEval,
+      prompt: `${options.prompt || '>'} `,
+      writer: writeFullDepth,
+    })
   }
 
   // this is the instance of the interpreter we return customized according
   // to the options they pass into the interpreter() creation function
   const theInterpreter = {
     evaluate,
+    getArg,
     globals,
     isSymbol,
     getFnSymbolForForm,
@@ -153,8 +189,12 @@ export const interpreter = (options = {}) => {
     const $fnSymbol = getFnSymbolForForm(sform)
 
     // special forms are called with their args unevaluated:
-    if ($fnSymbol && variables[$fnSymbol] && variables[$fnSymbol]._special) {
-      return variables[$fnSymbol](sform, variables, trace, theInterpreter)
+    if ($fnSymbol) {
+      const fname = symbolToString($fnSymbol)
+
+      if (variables[fname] && variables[fname]._special) {
+        return variables[fname](sform, variables, trace, theInterpreter)
+      }
     }
 
     // regular forms have their args evaluated before they are called
@@ -171,12 +211,16 @@ export const interpreter = (options = {}) => {
       evaluated = await mapAndWait(sform, atom => evaluate(atom, variables))
     }
 
-
     trace('evaluateArrayForm: evaluated before call:', JSON.stringify(evaluated, null, 2), evaluated[0], typeof evaluated[0])
     if (typeof evaluated[0] === 'function') {
       trace(`evaluateArrayForm: calling ${$fnSymbol}`)
 
-      return evaluated[0].call(undefined, evaluated, variables, trace, theInterpreter)
+      return evaluated[0]._formhandler ?
+        // form handler functions take the evaluated array form data
+        evaluated[0](evaluated, variables, trace, theInterpreter) :
+        // non-form handlers are plain javascript functions (e.g. console.log)
+        // the arguments follow the symbol e.g. ["$console.log", "hello"]
+        evaluated[0].apply(undefined, evaluated.slice(1))
     }
 
     trace(`evaluateArrayForm: ${$fnSymbol} not a function passing thru as evaluated data`)
@@ -196,9 +240,13 @@ export const interpreter = (options = {}) => {
     const $fnSymbol = getFnSymbolForForm(oform)
 
     if ($fnSymbol) {
-      if (variables[$fnSymbol] && variables[$fnSymbol]._special) {
-        return variables[$fnSymbol](oform, variables, trace, theInterpreter)
-      } else if (typeof variables[$fnSymbol] === 'function') {
+      // evaluate the symbol to a function
+      // note this resolves e.g. "$console.log" which a simple variables[fname] wouldn't find
+      const func = await evaluate($fnSymbol, variables)
+
+      if (func && func._special) {
+        return func(oform, variables, trace, theInterpreter)
+      } else if (typeof func === 'function') {
         // regular sforms have their args evaluated before they are called
         // the equivalent here is the values of all keys are evaluated before the fn is called
         // note: we also wait here for promises (if any) to settle before making the call
@@ -210,9 +258,20 @@ export const interpreter = (options = {}) => {
 
         trace(`evaluateObjectForm: calling ${$fnSymbol}`)
 
-        return variables[$fnSymbol].call(undefined, oform, variables, trace, theInterpreter)
+        const result = func._formhandler ?
+          // form handler functions take the evaluated object form data
+          func(oform, variables, trace, theInterpreter) :
+          // non-form handlers are plain javascript functions (e.g. console.log)
+          // the arguments are the array value of the symbol e.g. {"$console.log": ["hello"]}
+          func(...oform[$fnSymbol])
+
+        trace(`evaluateObjectForm: returning from calling ${$fnSymbol}`, result)
+
+        return result
       }
 
+      // TBD this should probably change to being an error...
+      // i.e. this is more likely a typo than an intention to pass thru data with $ in a key?
       trace(`evaluateObjectForm: passing thru object with unrecognized symbol key "${$fnSymbol}":`, JSON.stringify(oform, null, 2))
     }
 
